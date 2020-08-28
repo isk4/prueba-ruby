897502bfaa2bc409d9248058312da89fc97bea71
Victor Gutierrez <vgutierrezdiban@protonmail.com>
2020-08-28 12:04:49 -0400

    fix navbar component and modify web_page_builder.rb for testing

--- components.rb
+++ components.rb
@@ -26,14 +26,14 @@ end
 
 def navbar
     return(
-        "\t\t<nav class='navbar navbar-expand-lg navbar-dark bg-dark'>
+        "\t\t<nav class='navbar navbar-expand-lg navbar-dark bg-dark fixed-top'>
             <div class='container'>
                 <a class='navbar-brand' href='#top'>Curiosity Rover Photos</a>
                 <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNavAltMarkup' aria-controls='navbarNavAltMarkup' aria-expanded='false' aria-label='Toggle navigation'>
                     <span class='navbar-toggler-icon'></span>
                 </button>
                 <div class='collapse navbar-collapse' id='navbarNavAltMarkup'>
-                    <div class='navbar-nav mr-auto'>
+                    <div class='navbar-nav ml-auto'>
                         <a class='nav-link' href='https://github.com/chrisccerami/mars-photo-api'>API Documentation<span class='sr-only'>(current)</span></a>
                         <a class='nav-link' href='https://github.com/chrisccerami'>API's Creator Github</a>
                         <a class='nav-link' href='https://api.nasa.gov/'>Other NASA APIs</a>
--- web_page_builder.rb
+++ web_page_builder.rb
@@ -1,5 +1,11 @@
 require_relative 'request'
+require_relative 'components'
 
-def build_web_page(hash)
-    
+api_address = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=666&api_key="
+api_key = "B9038ToX0u4qHgFC6VdEOos65TANfKXGgHuyh3GU"
+
+def build_web_page
+    return "<html>\n" + head + body(navbar + "<h1>Prueba</h1>\n") + "</html>"
 end
+
+File.write("index.html", build_web_page)
